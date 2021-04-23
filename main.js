ad4c629a4f36c226e02222a5a5e8be767bf751a9
phsndev <78909025+phsndev@users.noreply.github.com>
2021-04-23 14:55:00 -0300

    Adiciona Front-end incompleto
    Adicionando algumas coisas básicas do front-end para depois alterar

---
+++ main.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+	el: '#app',
+	data: {
+		errorMsg: false,
+		successMsg: false,
+		tableAutor: true,
+		tableLivro: true,
+		tableEditora: true,
+		tableGenero: true,
+	}
+});
